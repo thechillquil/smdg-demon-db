905c306a49ab83a39943f1ffc45079f199563144
Jim Evans <james.h.evans.jr@gmail.com>
2021-12-25 09:50:36 -0500

    Add first part of styling for views

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ var compendiumRouter = require("./routes/compendium");
 
 app.use(express.json());
 
+app.use("/public", express.static("./public"));
+
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
 app.use("/compendium", compendiumRouter);
