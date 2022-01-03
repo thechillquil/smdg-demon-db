c93f1bac4b383585aa898ab2bc3db950e9f4ec5a
jimevans <james.h.evans.jr@gmail.com>
2022-01-03 10:56:53 -0500

    chore: Move CORS addition to middleware (#9)

--- src/app.js
+++ src/app.js
@@ -9,12 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const cors = require("./middleware/cors");
+app.use(cors);
 
 // Get global configuration
 const config = require("./config");
